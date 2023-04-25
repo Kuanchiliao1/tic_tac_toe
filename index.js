8570d4ec5d5eae14b2b6ed03a70c82639febaf54
kuanchiliao1 <kuanchiliao@gmail.com>
2023-04-25 11:44:34 -0400

    Set up factory and module skeletons

---
+++ index.js
@@ -0,0 +1,15 @@
+const Gameboard = (function() {
+  return ["X", "X", "X", "X", "X", "X", "X", "X", "X", ]
+})()
+
+const PlayerFactory = (name, marker) => {
+  return {name, marker}
+}
+
+const DisplayController = (function() {
+  const renderGameboard = () => {
+
+  }
+
+  return {renderGameboard}
+})()
