9f9670a5846fa9f3b0d1699b7f9e7586deed3e57
kuanchiliao1 <kuanchiliao@gmail.com>
2023-04-25 12:04:40 -0400

    Render grid cells with JS

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const Gameboard = (function() {
-  return ["X", "X", "X", "X", "X", "X", "X", "X", "X", ]
+  return ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
 })()
 
 const PlayerFactory = (name, marker) => {
+  const markSpot = (gridElement) => {
+    gridElement.textContent = marker
+  }
+
   return {name, marker}
 }
 
 const DisplayController = (function() {
-  const renderGameboard = () => {
+  const playerOne = PlayerFactory("Tony", "O")
 
+  const renderGameboard = () => {
+    const gameboardEl = document.getElementById("gameboard")
+
+    for (let i = 0; i < Gameboard.length; i++) {
+      const cellEl = document.createElement("div")
+      cellEl.classList.add("gameboard__cell")
+      cellEl.textContent = Gameboard[i]
+      gameboardEl.append(cellEl)
+    }
   }
 
   return {renderGameboard}
 })()
+
+DisplayController.renderGameboard()
