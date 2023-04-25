ba4bf6c9791201760cac6d8ac9558c1c2cb90b37
kuanchiliao1 <kuanchiliao@gmail.com>
2023-04-25 13:00:20 -0400

    Write basic game logic

--- index.js
+++ index.js
@@ -1,30 +1,58 @@
-const Gameboard = (function() {
-  return ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
-})()
+const Gameboard = (function () {
+  return ['', '', '', '', '', '', '', '', ''];
+})();
 
 const PlayerFactory = (name, marker) => {
-  const markSpot = (gridElement) => {
-    gridElement.textContent = marker
-  }
+  const markSpot = function (gridElement, otherPlayer) {
+    const cellId = gridElement.id;
+    const isCellEmpty = !Gameboard[cellId];
 
-  return {name, marker}
-}
+    if (isCellEmpty) {
+      Gameboard[cellId] = marker;
+      this.isCurrentPlayer = false;
+      otherPlayer.isCurrentPlayer = true;
+    } else {
+      alert('pick something else!');
+    }
+    console.log(cellId);
+    DisplayController.renderGameboard();
+  };
 
-const DisplayController = (function() {
-  const playerOne = PlayerFactory("Tony", "O")
+  const isCurrentPlayer = false;
+
+  return { name, marker, isCurrentPlayer, markSpot };
+};
+
+const DisplayController = (function () {
+  const playerOne = PlayerFactory('Tony', 'O');
+  const playerTwo = PlayerFactory('Sam', 'X');
+  const gameboardEl = document.getElementById('gameboard');
+
+  const addEventListeners = () => {
+    gameboardEl.addEventListener('click', (event) => {
+      const cellEl = event.target;
+      if (playerOne.isCurrentPlayer) {
+        playerOne.markSpot(cellEl, playerTwo);
+      } else {
+        playerTwo.markSpot(cellEl, playerOne);
+      }
+    });
+  };
 
   const renderGameboard = () => {
-    const gameboardEl = document.getElementById("gameboard")
+    gameboardEl.innerHTML = '';
 
     for (let i = 0; i < Gameboard.length; i++) {
-      const cellEl = document.createElement("div")
-      cellEl.classList.add("gameboard__cell")
-      cellEl.textContent = Gameboard[i]
-      gameboardEl.append(cellEl)
+      const cellEl = document.createElement('div');
+      cellEl.classList.add('gameboard__cell');
+      cellEl.id = i;
+      cellEl.textContent = Gameboard[i];
+      gameboardEl.append(cellEl);
     }
-  }
+  };
 
-  return {renderGameboard}
-})()
+  return { renderGameboard, addEventListeners };
+})();
 
-DisplayController.renderGameboard()
+DisplayController.addEventListeners();
+DisplayController.renderGameboard();
